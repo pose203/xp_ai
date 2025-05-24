fb114ea0e166b815bb697a022aef89a7fb2491e6
pose203 <18720859243@163.com>
2025-05-24 16:27:59 +0800

    腾讯面试题

---
+++ batjtmd/js/str/3.js
@@ -0,0 +1,23 @@
+// 包装类
+
+let a = "abc";
+let b = new String("abc");
+console.log(a == b);// 值
+// js 给所有简单数据类型提供了 相应类型的类 包装类
+console.log(a === b);// 类型不同
+console.log(b.split(''));
+// 为了统一面向对象写法
+// js 会主动把简单数据类型包装成对象
+//a -> new String(a)
+console.log(a.split(''));
+// 之后会销毁对象，回归原来的简单类型
+
+
+
+
+
+
+
+
+
+

d65d2a488ae9a8e6cb83a7c1b8a42636b8798116
pose203 <18720859243@163.com>
2025-07-01 15:04:02 +0800

    more

---
+++ html5/localStorage/common.js
@@ -0,0 +1,11 @@
+const addItems = document.querySelector('.add-items');//form
+const itemsList = document.querySelector('.plates');// 列表
+
+function addItem(e){
+    e.preventDefault();
+    
+
+}   
+
+addItems.addEventListener('submit',addItem);
+
